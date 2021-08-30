75c018215d55c279400ad3b6e31065e15918d719
aoigj100a <aoigj100@gmail.com>
2021-08-30 16:43:52 +0800

    view init

---
+++ src/component/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import TelBlacklist from "./TelBlacklist";
+
+function App() {
+  return (
+    <TelBlacklist />
+  );
+}
+
+export default App;
---
+++ src/component/TelBlacklist.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TelBlacklist() {
+  return (
+    <div>TelBlacklist</div>
+  );
+}
+
+export default TelBlacklist;
