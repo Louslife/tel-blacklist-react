b34e9fb3de690f17bd096c51f2956cd3ce0e8a8a
aoigj100a <aoigj100@gmail.com>
2021-08-31 16:34:33 +0800

    feat:add tel

--- src/component/TelBlacklist.js
+++ src/component/TelBlacklist.js
@@ -5,7 +5,7 @@ import "../css/basic.css";
 
 function TelBlacklist() {
   // 電話名單有 黑名單 與 白名單
-  // 狀態：telList(藉由 API 撈回 list)、白名單、黑名單、正在修改
+  // 狀態：telList(藉由 API 撈回 list)、IsUpdate、IsBlack、isAdd
 
   const data = [
     {
@@ -30,15 +30,21 @@ function TelBlacklist() {
       tel: "09",
     }
   ]
+
   const [telList, setTelList] = useState(null);
+  const [isUpdate, setIsUpdate] = useState(false);
+  // const [isBlack, setIsBlack] = useState(true);
+  const [isAdd, setIsAdd] = useState(false);
+
+  const [newTel, setNewTel] = useState("");
+  const [newId, setNewId] = useState("");
+  const [newUser, setNewUser] = useState("");
 
   // 拉取資料時將程式碼包覆於 useEffect 在 render 之後執行一次
   useEffect(() => {
     // 在這裏拉取資料
     setTelList(data)
   }, []);
-
-  console.table(telList)
 
   const hendleSeachTel = () => {
     // 搜尋功能
@@ -49,7 +55,26 @@ function TelBlacklist() {
   const hendleCreateTel = () => {
     // 新增一筆電話
     // 在這裏認證一筆電話是否合法
+    // IsAdd === true 時 input會出現
+    if (isAdd) {
+      setIsAdd(false)
+    } else {
+      setIsAdd(true)
+    }
+
+  }
+
+  const hendleSaveTel = () => {
+    // 儲存一筆電話
+    // 若我按下外框或者 enter 將存擋並且 setIsAdd(false)
     // cell Create API
+    const newList = [...telList, {
+      telId: newId,
+      telName: newUser,
+      tel: newTel,
+    }]
+    setIsAdd(false)
+    setTelList(newList)
   }
 
   const hendleUpdateTel = () => {
@@ -58,6 +83,11 @@ function TelBlacklist() {
     // 驗證被修改的電話是否合法
     // cell Update API
     // 修改結束之後可以使用點選外框或者 Enter 來存擋
+    if (isUpdate) {
+      setIsUpdate(false)
+    } else {
+      setIsUpdate(true)
+    }
   }
 
   const hendleDeleteTel = () => {
@@ -71,6 +101,23 @@ function TelBlacklist() {
     // 白名單變成黑名單
     // 黑名單變成白名單
   }
+
+  const getTelInputValue = (e) => {
+
+    setNewTel(e.target.value);
+    console.log(e.target.value)
+  }
+  const getIdInputValue = (e) => {
+
+    setNewId(e.target.value);
+    console.log(e.target.value)
+  }
+  const getUserInputValue = (e) => {
+
+    setNewUser(e.target.value);
+    console.log(e.target.value)
+  }
+
 
   return (
     <>
@@ -86,7 +133,8 @@ function TelBlacklist() {
         <button>顯示全部</button>
       </nav>
 
-      <div className={styles.addTel}>+</div>
+      <div className={styles.addTel}
+        onClick={hendleCreateTel}>+</div>
 
       <main className="container">
         <div className="row">
@@ -95,17 +143,37 @@ function TelBlacklist() {
           <div className="col">電話</div>
           <div className="col">行動</div>
         </div>
+        
         {(telList ? telList.map((obj, index) =>
-          <div className="row">
-            <div className="col tel-id">{obj.telId}</div>
-            <div className="col tel-name">{obj.telName}</div>
-            <div className="col tel">{obj.tel}</div>
+          <div className="row" key={index}>
+            {isUpdate ? <div className="col"><input type="text" value={obj.telId}/></div> : <div className="col tel-id"> {obj.telId}</div>}
+            {isUpdate ? <div className="col"><input type="text" value={obj.telName}/></div> : <div className="col tel-name"> {obj.telName}</div>}
+            {isUpdate ? <div className="col"><input type="text" value={obj.tel}/></div> : <div className="col tel"> {obj.tel}</div>}
             <div className="col tel-action">
-              <div className={styles.telActionRevise}><i>修改</i></div>
+              <div className={styles.telActionRevise}
+                onClick={hendleUpdateTel}><i>修改</i></div>
               <div className={styles.telActionDel}><i>刪除</i></div>
             </div>
           </div>) : "Get No Data"
         )}
+
+        
+
+        {isAdd ? <div className="row">
+          <div className="col"><input name="addId" type="text"
+            // value={newId}
+            onChange={getIdInputValue} /></div>
+          <div className="col"><input name="addUser" type="text"
+            // value={newUser}
+            onChange={getUserInputValue} /></div>
+          <div className="col"><input name="addtel" type="text"
+            // value={newTel}
+            onChange={getTelInputValue} /></div>
+          <div className="col tel-action">
+            <div className={styles.telActionDel}
+              onClick={hendleSaveTel}><i>確定</i></div>
+          </div>
+        </div> : ""}
       </main>
     </>
   )
